b2ce2d7804eedfd4f269c402a9c78901e64a1372
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2024-01-15 13:43:29 +0530

    Add files via upload

---
+++ Switch_case_in_C#/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_case_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a day of the week:");
+
+            string day = Console.ReadLine();
+
+            switch (day.ToLower()) // Convert the input to lowercase for case-insensitive comparison
+            {
+                case "monday":
+                    Console.WriteLine("It's the start of the week!");
+                    break;
+
+                case "tuesday":
+                case "wednesday":
+                case "thursday":
+                    Console.WriteLine("It's a weekday.");
+                    break;
+
+                case "friday":
+                    Console.WriteLine("TGIF! It's Friday!");
+                    break;
+
+                case "saturday":
+                case "sunday":
+                    Console.WriteLine("It's the weekend!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input. Please enter a valid day of the week.");
+                    break;
+            }
+            Console.ReadLine();
+            }
+    }
+}
