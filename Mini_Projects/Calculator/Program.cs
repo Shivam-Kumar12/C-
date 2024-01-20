86908c8fb5405e1687d19b0fa0a0ca78dddbe2e4
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2024-01-20 09:32:27 +0530

    Add files via upload

---
+++ Mini_Projects/Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            string answer;
+            int result;
+            Console.WriteLine("Hello,Welcome To The calculator Program");
+            Console.WriteLine("Please Enter You first Number");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter You Second Number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What Type of operation would You like To Do");
+            Console.WriteLine("please Enter A for Addition S for subtraction M for Multiply D for Divide");
+            answer=Console.ReadLine();
+
+            if(answer == "A") 
+            {
+                result = num1 + num2;
+                Console.WriteLine($"The Result is {num1} and {num2} is:"+result);
+            }
+            else if(answer=="S")
+            {
+                result = num1 - num2;
+                Console.WriteLine($"The Result is {num1} and {num2} is:" + result);
+            }
+            else if (answer == "M")
+            {
+                result = num1 * num2;
+                Console.WriteLine($"The Result is {num1} and {num2} is:" + result);
+            }
+            else if (answer == "D")
+            {
+                result = num1 / num2;
+                Console.WriteLine($"The Result is:" + result);
+            }
+            Console.WriteLine("Thanku for Using the calculator");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Mini_Projects/Simple_Dice_game/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Dice_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerrandom;
+            int enemyrandom;
+            int playerpoints=0;
+            int enemypoints=0;
+            Random rand = new Random();
+
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine("Press any Key to roll the dice");
+                Console.ReadKey();
+                Console.WriteLine("Enter The no by user: ");
+                playerrandom = rand.Next(1, 7);
+                Console.WriteLine("you rolled a: " + playerrandom);
+
+                Console.WriteLine(".....");
+                System.Threading.Thread.Sleep(1000);
+
+
+                enemyrandom = rand.Next(1, 7);
+                Console.WriteLine("Enemy AI Rolled a :" + enemyrandom);
+
+                if (playerrandom > enemyrandom)
+                {
+                    playerpoints++;
+                    Console.WriteLine("Player Win The round");
+                }
+                else if (playerrandom < enemyrandom) {
+                    enemypoints++;
+                    Console.WriteLine("Enemy wins this round");
+                }
+                else
+                {
+                    Console.WriteLine("Draw");
+                }
+                Console.WriteLine("The score is now - player: "+ playerpoints + " and - Enemy:"+enemypoints);
+            }
+       
+            Console.ReadKey();
+        }
+    }
+}
